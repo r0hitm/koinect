cbfba898e2047eee69f26ff1666e1a87c1d9d3be
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-11-04 16:33:21 +0530

    basic register & login page ready

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,57 @@
+import { useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+
 function Login() {
+    const user = useAuth();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+
+    const handleLogin = event => {
+        event.preventDefault();
+        setLoading(true);
+        user.login(email, password)
+            .then(() => {
+                setLoading(false);
+            })
+            .catch(error => {
+                setError(error.message);
+                setLoading(false);
+            });
+    };
+
     return (
-        <div>
+        <section>
             <h1>Login</h1>
-        </div>
+            <form onSubmit={handleLogin}>
+                <div>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={event => setEmail(event.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={event => setPassword(event.target.value)}
+                        required
+                    />
+                </div>
+                {error && <p>{error}</p>}
+                <div>
+                    <button className="button" type="submit" disabled={loading}>
+                        Login
+                    </button>
+                </div>
+            </form>
+        </section>
     );
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,71 @@
+import { useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+
 function Register() {
+    const user = useAuth();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirm, setConfirm] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const handleRegister = event => {
+        event.preventDefault();
+        setLoading(true);
+        if (password !== confirm) {
+            setError("Passwords do not match");
+            setLoading(false);
+            return;
+        }
+        user.register(email, password)
+            .then(() => {
+                setLoading(false);
+            })
+            .catch(error => {
+                setError(error.message);
+                setLoading(false);
+            });
+    };
+
     return (
-        <div>
+        <section>
             <h1>Register</h1>
-        </div>
+            <form onSubmit={handleRegister}>
+                <div>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={event => setEmail(event.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={event => setPassword(event.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <input
+                        type="password"
+                        placeholder="Confirm Password"
+                        onChange={event => setConfirm(event.target.value)}
+                        required
+                    />
+                </div>
+                {error && <p>{error}</p>}
+                <div>
+                    <button className="button" type="submit" disabled={loading}>
+                        Register
+                    </button>
+                </div>
+            </form>
+        </section>
     );
 }
 
