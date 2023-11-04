288cb4ebe56f01a265926abdf8049e72f191030e
Manmeet Singh <manmeets.zsh@gmail.com>
2023-11-04 20:35:00 +0530

    initial design commit

---
+++ src/components/PrimaryButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PrimaryButton = () => {
+  return( 
+  <>
+    <button className="primaryButton" >
+      Add New
+      </button>
+  </>
+  )
+}
+
+export default PrimaryButton;
