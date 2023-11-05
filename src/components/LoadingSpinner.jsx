2f601e765e0d041cff31d681ea5041d0795bbf52
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-11-05 07:33:52 +0530

    merge

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,11 +1,13 @@
 // Loading spinner to show when the login/register/logout is happening
-import { useAuth } from "../hooks/useAuth";
+// import { useAuth } from "../hooks/useAuth";
 
 export default function LoadingSpinner() {
-    const { loading } = useAuth();
-    return loading ? (
+    // const { loading } = useAuth();
+    return (
         <div className="loadingSpinner">
+            <span className="loader"></span>{" "}
+            {/*This is spinner component to be added. */}
             <div className="spinner"></div>
         </div>
-    ) : null;
+    );
 }
