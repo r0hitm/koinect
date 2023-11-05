c8bf1f66bfd5c3dc21ea42b4daa8dfc379f50833
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-11-05 07:38:30 +0530

    loading spinner interactivity w/o spinner

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useAuth } from "../hooks/useAuth";
 import { useNavigate, Link } from "react-router-dom";
 import AppHeader from "../components/AppHeader";
+import LoadingSpinner from "../components/LoadingSpinner";
 function Login() {
     const user = useAuth();
 
@@ -30,32 +31,56 @@ function Login() {
             });
     };
 
-    return (
-        <section>
-            <AppHeader />
-           <div className="loginForm">
-           <h1>Log into your account</h1>
-            <form onSubmit={handleLogin}>
-                <div>
-                    <input className="dataPlaceholder" type="email" placeholder="Email" defaultValue={email} onChange={event => setEmail(event.target.value)} required />
+    if (loading) {
+        return <LoadingSpinner />;
+    } else {
+        return (
+            <section>
+                <AppHeader />
+                <div className="loginForm">
+                    <h1>Log into your account</h1>
+                    <form onSubmit={handleLogin}>
+                        <div>
+                            <input
+                                className="dataPlaceholder"
+                                type="email"
+                                placeholder="Email"
+                                defaultValue={email}
+                                onChange={event => setEmail(event.target.value)}
+                                required
+                            />
+                        </div>
+                        <div>
+                            <input
+                                className="dataPlaceholder"
+                                type="password"
+                                placeholder="Password"
+                                defaultValue={password}
+                                onChange={event =>
+                                    setPassword(event.target.value)
+                                }
+                                required
+                            />
+                        </div>
+                        {error && <p>{error}</p>}
+                        <div>
+                            <button
+                                className="loginBtn primaryButton"
+                                type="submit"
+                                disabled={loading}
+                            >
+                                Login
+                            </button>
+                        </div>
+                        <div>
+                            Don&apos;t have an account?{" "}
+                            <Link to="/register">Register Here</Link>
+                        </div>
+                    </form>
                 </div>
-                <div>
-                    <input className="dataPlaceholder" type="password" placeholder="Password" defaultValue={password} onChange={event => setPassword(event.target.value)} required
-                    />
-                </div>
-                {error && <p>{error}</p>}
-                <div>
-                    <button className="loginBtn primaryButton" type="submit" disabled={loading}>
-                        Login
-                    </button>
-                </div>
-                <div>
-                    Don&apos;t have an account? <Link to="/register">Register Here</Link>
-                </div>
-            </form>
-           </div>
-        </section>
-    );
+            </section>
+        );
+    }
 }
 
 export default Login;
