d52dd2c8746674d8adde5a5f361ee4ab452cd8a1
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-11-05 07:44:04 +0530

    spinner working

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,5 +1,5 @@
 // Loading spinner to show when the login/register/logout is happening
-// import { useAuth } from "../hooks/useAuth";
+import Spinner from "../assets/spinner.gif";
 
 export default function LoadingSpinner() {
     // const { loading } = useAuth();
@@ -7,7 +7,9 @@ export default function LoadingSpinner() {
         <div className="loadingSpinner">
             <span className="loader"></span>{" "}
             {/*This is spinner component to be added. */}
-            <div className="spinner"></div>
+            <div className="spinner">
+                <img src={Spinner} alt="Loading" />
+            </div>
         </div>
     );
 }
